cb9d30a09d745efaa284cd19f4f33a25cd466b93
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-01-27 12:48:25 +0200

    add object literal demo code

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+let name = 'Player 1';
+let health = 30;
 
-document.addEventListener("keydown", evt => {
-    if (evt.code === 'ArrowRight') {
-        console.log("Right", move(10));
-    }
+let player = {
+    name : name,
+    health: health,
+}
 
-    if (evt.code === 'ArrowLeft') {
-        console.log("Left", move(10, -1));
-    }
 
-    if (evt.code === 'ArrowUp') {
-        console.log("Up", move(10));
-    }
 
-    if (evt.code === 'ArrowDown') {
-        console.log("Down", move(10, -1));
-    }
-});
+
+
+
+
+
+
+
+// sayMyName: function() {
+    //         console.log(this.name);
+    //     },
+    
+// [extention]:
