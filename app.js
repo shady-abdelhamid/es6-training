f9fd65b78a1da267fa53f2c3b356b778a416accc
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-01-27 18:03:21 +0200

    add template literal demo code

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-
-let  agumonForms = [
-    'Botamon',
-    'Koromon',
-    'Agumon',
-    'Greymon', 
-    'MetalGreymon', 
-    'WarGreymon'
-];
+let name = '';
 
 
 
@@ -14,8 +6,44 @@ let  agumonForms = [
 
 
 
-let gabumon = {
-    level: "rockie",
-    type: "reptile",
-    exp: 15,
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                            
+                                                            
+// EEEEEEEEEEEEEEEEEEEEEE   SSSSSSSSSSSSSSS         66666666   
+// E::::::::::::::::::::E SS:::::::::::::::S       6::::::6    
+// E::::::::::::::::::::ES:::::SSSSSS::::::S      6::::::6     
+// EE::::::EEEEEEEEE::::ES:::::S     SSSSSSS     6::::::6      
+//   E:::::E       EEEEEES:::::S                6::::::6       
+//   E:::::E             S:::::S               6::::::6        
+//   E::::::EEEEEEEEEE    S::::SSSS           6::::::6         
+//   E:::::::::::::::E     SS::::::SSSSS     6::::::::66666    
+//   E:::::::::::::::E       SSS::::::::SS  6::::::::::::::66  
+//   E::::::EEEEEEEEEE          SSSSSS::::S 6::::::66666:::::6 
+//   E:::::E                         S:::::S6:::::6     6:::::6
+//   E:::::E       EEEEEE            S:::::S6:::::6     6:::::6
+// EE::::::EEEEEEEE:::::ESSSSSSS     S:::::S6::::::66666::::::6
+// E::::::::::::::::::::ES::::::SSSSSS:::::S 66:::::::::::::66 
+// E::::::::::::::::::::ES:::::::::::::::SS    66:::::::::66   
+// EEEEEEEEEEEEEEEEEEEEEE SSSSSSSSSSSSSSS        666666666
