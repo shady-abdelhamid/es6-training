37effa69a779b512b17261948e01ac8177d644ae
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-01-30 09:30:19 +0200

    add destructuring demo code

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-let name = '';
+/**
+ * Array destructuring
+ */
+let weapons = ['AWM', 'AUG', 'Skorpion'];
 
 
 
@@ -8,42 +11,16 @@ let name = '';
 
 
 
+ /**
+ * Object destructuring
+ */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                                            
-                                                            
-// EEEEEEEEEEEEEEEEEEEEEE   SSSSSSSSSSSSSSS         66666666   
-// E::::::::::::::::::::E SS:::::::::::::::S       6::::::6    
-// E::::::::::::::::::::ES:::::SSSSSS::::::S      6::::::6     
-// EE::::::EEEEEEEEE::::ES:::::S     SSSSSSS     6::::::6      
-//   E:::::E       EEEEEES:::::S                6::::::6       
-//   E:::::E             S:::::S               6::::::6        
-//   E::::::EEEEEEEEEE    S::::SSSS           6::::::6         
-//   E:::::::::::::::E     SS::::::SSSSS     6::::::::66666    
-//   E:::::::::::::::E       SSS::::::::SS  6::::::::::::::66  
-//   E::::::EEEEEEEEEE          SSSSSS::::S 6::::::66666:::::6 
-//   E:::::E                         S:::::S6:::::6     6:::::6
-//   E:::::E       EEEEEE            S:::::S6:::::6     6:::::6
-// EE::::::EEEEEEEE:::::ESSSSSSS     S:::::S6::::::66666::::::6
-// E::::::::::::::::::::ES::::::SSSSSS:::::S 66:::::::::::::66 
-// E::::::::::::::::::::ES:::::::::::::::SS    66:::::::::66   
-// EEEEEEEEEEEEEEEEEEEEEE SSSSSSSSSSSSSSS        666666666
+ let player = {
+     name: 'ShadoWolf654',
+     rank: 'Diamond',
+     rb: 72,
+     position: [123, 456],
+     getPosition() {
+         return this.position
+     }
+ }
