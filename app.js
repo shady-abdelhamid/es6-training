d3f780e326d72e548fc381ed60663ff464668a64
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-02-02 10:17:10 +0200

    add generators demo code

--- app.js
+++ app.js
@@ -1,45 +1,13 @@
-/**
- * iterators
- */
+function *selectHero() {
+    yield 'Ironman';
+    yield 'Spiderman';
+}
 
-let array = [1, 2, 3];
-
-let it = array[Symbol.iterator]();
-
+let it = selectHero();
+console.log(it.next());
+console.log(it.next());
 console.log(it.next());
 
 
 
 
-/** iterateable object */
-
-// let enemy = {
-//     name: 'Darth Vader',
-//     abilities: ['Force Valor', 'Telekinesis', 'Master Lightsaber Duelist'],
-//     [Symbol.iterator]: function() {
-//         let i = 0;
-//         let abilities = this.abilities;
-//         return {
-//             next: function() {
-//                 let value = abilities[i];
-//                 i++;
-//                 return {
-//                     done: i > abilities.length,
-//                     value
-//                 };
-//             }
-//         };
-//     }
-// };
-
-// for (const ability of enemy) {
-//     console.log(ability);
-// }
-
-
-
-
-
-/**
- * 
- */
