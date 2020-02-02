641c6d3c500cb91727f438656e79a9ff062d6ead
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-02-02 12:17:37 +0200

    update iterators

--- app.js
+++ app.js
@@ -7,39 +7,33 @@ let array = [1, 2, 3];
 let it = array[Symbol.iterator]();
 
 console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
 
 
 
 
 /** iterateable object */
 
-// let enemy = {
-//     name: 'Darth Vader',
-//     abilities: ['Force Valor', 'Telekinesis', 'Master Lightsaber Duelist'],
-//     [Symbol.iterator]: function() {
-//         let i = 0;
-//         let abilities = this.abilities;
-//         return {
-//             next: function() {
-//                 let value = abilities[i];
-//                 i++;
-//                 return {
-//                     done: i > abilities.length,
-//                     value
-//                 };
-//             }
-//         };
-//     }
-// };
+let enemy = {
+    name: 'Darth Vader',
+    abilities: ['Force Valor', 'Telekinesis', 'Master Lightsaber Duelist'],
+    [Symbol.iterator]: function() {
+        let i = 0;
+        return {
+            next: () => {
+                let value = this.name + this.abilities[i];
+                i++;
+                return {
+                    done: i > this.abilities.length,
+                    value
+                };
+            }
+        };
+    }
+};
 
-// for (const ability of enemy) {
-//     console.log(ability);
-// }
-
-
-
-
-
-/**
- * 
- */
+for (const ability of enemy) {
+    console.log(ability);
+}
